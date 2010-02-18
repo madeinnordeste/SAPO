2d14037d8230019578e11282a2f6528f376e1f91
madeinnordeste <madeinnordeste@gmail.com>
2010-02-18 21:50:25 -0200

    Mais bugs corrigidos

--- application/logs/2010-02-18.log.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-02-18 18:00:43 -03:00 --- error: Não foi possível capturar  Kohana_404_Exception: A página <tt>static/images/bg-user-info.png</tt> requisitada, não foi encontrada. no arquivo system/core/Kohana.php, linha 841
-2010-02-18 18:00:44 -03:00 --- error: Não foi possível capturar  Kohana_404_Exception: A página <tt>static/images/bg-user-info.png</tt> requisitada, não foi encontrada. no arquivo system/core/Kohana.php, linha 841
-2010-02-18 18:00:44 -03:00 --- error: Não foi possível capturar  Kohana_404_Exception: A página <tt>static/images/bg-user-info.png</tt> requisitada, não foi encontrada. no arquivo system/core/Kohana.php, linha 841
-2010-02-18 18:04:48 -03:00 --- error: Não foi possível capturar  Kohana_404_Exception: A página <tt>usuarios/meu_cadastro</tt> requisitada, não foi encontrada. no arquivo system/core/Kohana.php, linha 841
-2010-02-18 19:58:38 -03:00 --- error: Não foi possível capturar  Kohana_Exception: Método <tt>procedimentos</tt> inválido, chamado por <tt>Advogado_Model</tt>. no arquivo system/libraries/ORM.php, linha 257
